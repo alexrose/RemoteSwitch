64545941d7cefd2670027af20a9b6b428b390fd3
Alex Trandafir <alex.itcv@gmail.com>
2017-11-30 00:32:52 +0200

    First commit

---
+++ app/src/main/java/ro/itcv/alex/remoteswitch/MainActivity.java
@@ -0,0 +1,141 @@
+package ro.itcv.alex.remoteswitch;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ProgressDialog progress;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        try {
+            URL url = new URL("https://dev.itcv.ro/wemos.php");
+            new GetClass(this, url).execute();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    private class GetClass extends AsyncTask<String, Void, Void> {
+
+        private final Context context;
+        private final URL url;
+
+        public GetClass(Context c, URL url){
+            this.context = c;
+            this.url = url;
+        }
+
+        protected void onPreExecute(){
+            progress= new ProgressDialog(this.context);
+            progress.setMessage("Loading");
+            progress.show();
+        }
+
+        @Override
+        protected Void doInBackground(String... params) {
+            try {
+                final TextView outputView = (TextView) findViewById(R.id.switchOneStatus);
+
+                HttpURLConnection connection = (HttpURLConnection)this.url.openConnection();
+                connection.setRequestMethod("GET");
+                connection.setRequestProperty("USER-AGENT", "Mozilla/5.0");
+                connection.setRequestProperty("ACCEPT-LANGUAGE", "en-US,en;0.5");
+
+                BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+                String line = "";
+                StringBuilder responseOutput = new StringBuilder();
+                while((line = br.readLine()) != null ) {
+                    responseOutput.append(line);
+                }
+                br.close();
+
+                JSONObject jObject = new JSONObject(responseOutput.toString());
+                final String switchOneStatus = jObject.getString("switchOne");
+
+                MainActivity.this.runOnUiThread(new Runnable() {
+
+                    @Override
+                    public void run() {
+                        outputView.setText(switchOneStatus);
+                        progress.dismiss();
+                    }
+                });
+            } catch (MalformedURLException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+    }
+
+    public void turnSwitchOneOn(View View) {
+        try {
+            URL url = new URL("https://dev.itcv.ro/wemos.php");
+            new GetClass(this, url).execute();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void turnSwitchOneOff(View View) {
+        try {
+            URL url = new URL("https://dev.itcv.ro/wemos.php");
+            new GetClass(this, url).execute();
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+    }
+}
